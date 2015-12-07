2731cd852a8154cd38c24eee85b1a321554019ba
Veselin Georgiev <anrieff@gmail.com>
2015-12-07 20:34:58 +0200

    Add support for making screenshots (use the F12 key).

--- src/sdl.cpp
+++ src/sdl.cpp
@@ -24,6 +24,7 @@
 #include <SDL/SDL.h>
 #include <stdio.h>
 #include "sdl.h"
+#include "bitmap.h"
 
 SDL_Surface* screen = NULL;
 
@@ -62,6 +63,47 @@ void displayVFB(Color vfb[VFB_MAX_SIZE][VFB_MAX_SIZE])
 	SDL_Flip(screen);
 }
 
+// find an unused file name like 'quad_damage_0005.bmp'
+static void findUnusedFN(char fn[], const char* suffix)
+{
+	char temp[256];
+	int idx = 0;
+	while (idx < 10000) {
+		sprintf(temp, "quad_damage_%04d.bmp", idx);
+		FILE* f = fopen(temp, "rb");
+		if (!f) {
+			sprintf(temp, "quad_damage_%04d.exr", idx);
+			f = fopen(temp, "rb");
+		}
+		if (!f) break; // file doesn't exist - use that
+		fclose(f);
+		idx++;
+	}
+	sprintf(fn, "quad_damage_%04d.%s", idx, suffix); 
+}
+
+bool takeScreenshot(const char* filename)
+{
+	extern Color vfb[VFB_MAX_SIZE][VFB_MAX_SIZE]; // from main.cpp
+	
+	Bitmap bmp;
+	bmp.generateEmptyImage(frameWidth(), frameHeight());
+	for (int y = 0; y < frameHeight(); y++)
+		for (int x = 0; x < frameWidth(); x++)
+			bmp.setPixel(x, y, vfb[y][x]);
+	bool res = bmp.saveImage(filename);
+	if (res) printf("Saved a screenshot as `%s'\n", filename);
+	else printf("Failed to take a screenshot\n");
+	return res;
+}
+
+bool takeScreenshotAuto(Bitmap::OutputFormat fmt)
+{
+	char fn[256];
+	findUnusedFN(fn, fmt == Bitmap::outputFormat_BMP ? "bmp" : "exr");
+	return takeScreenshot(fn);
+}
+
 /// waits the user to indicate he wants to close the application (by either clicking on the "X" of the window,
 /// or by pressing ESC)
 void waitForUserExit(void)
@@ -77,6 +119,13 @@ void waitForUserExit(void)
 					switch (ev.key.keysym.sym) {
 						case SDLK_ESCAPE:
 							return;
+						case SDLK_F12:
+							// Shift+F12: screenshot in EXR; else, do it in (gamma-compressed) BMP.
+							if (ev.key.keysym.mod & (KMOD_LSHIFT | KMOD_RSHIFT))
+								takeScreenshotAuto(Bitmap::outputFormat_EXR);
+							else
+								takeScreenshotAuto(Bitmap::outputFormat_BMP);
+							break;
 						default:
 							break;
 					}
